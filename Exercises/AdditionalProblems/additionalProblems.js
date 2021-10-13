9e4535ffee3bdfe79a42b33c29754aad726351e8
erikthered14 <eric.chiu@galvanize.com>
2021-10-12 19:27:03 -0500

    (FEATURE) Add additional 3 problems

---
+++ Exercises/AdditionalProblems/additionalProblems.js
@@ -0,0 +1,27 @@
+// Problem 1
+const loopToFive = () => {
+  for (let i = 0; i < 5; i += 1) {
+    console.log(i);
+  }
+}
+
+
+// Problem 2
+const addIfEven = num => {
+  if(num % 2 = 0) {
+      return num + 5;
+  }
+  return num;
+}
+
+// Problem 3
+const displayEvenNumbers () {
+  let numbers = [1,2,3,4,5,6,7,8];
+    let evenNumbers = [];
+    for(let i=0; i<numbers.length-1; i++;){
+        if(numbers % 2 = 0); {
+            evenNumbers.push(i);
+        }
+        return evenNumbers;
+    }
+}
